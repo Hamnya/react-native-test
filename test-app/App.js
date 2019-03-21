8c32b88637b060f75885b71745a4111a5e174e9a
Hamnya <ihamnya@gmail.com>
2019-03-22 03:08:24 +0900

    move circle with button

--- test-app/App.js
+++ test-app/App.js
@@ -1,11 +1,79 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  NativeModules,
+  LayoutAnimation,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+const { UIManager } = NativeModules;
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 
 export default class App extends React.Component {
+  state = {
+    x: 0,
+    y: 0,
+  };
+
+  _onPressTop = () => {
+    // Animate the update
+    LayoutAnimation.spring();
+    this.setState({x: this.state.x - 15})
+  }
+
+  _onPressBottom = () => {
+    // Animate the update
+    LayoutAnimation.spring();
+    this.setState({x: this.state.x + 15})
+  }
+
+  _onPressLeft = () => {
+    // Animate the update
+    LayoutAnimation.spring();
+    this.setState({y: this.state.y - 15})
+  }
+
+  _onPressRight = () => {
+    // Animate the update
+    LayoutAnimation.spring();
+    this.setState({y: this.state.y + 15})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>얄루</Text>
+        <View style={[styles.box, {top: this.state.x, left: this.state.y}]} />
+
+
+
+
+          <View style = { styles.inputContainer}>
+            <TouchableOpacity onPress={this._onPressLeft}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>←</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this._onPressTop}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>↑</Text>
+              </View>
+            </TouchableOpacity>
+              <TouchableOpacity onPress={this._onPressBottom}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>↓</Text>
+              </View>
+            </TouchableOpacity>
+
+              <TouchableOpacity onPress={this._onPressRight}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>→</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
       </View>
     );
   }
@@ -14,8 +82,30 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  box: {
+    width: 100,
+    height: 100,
+    borderRadius: 100/2,
+    backgroundColor: 'red',
+  },
+  button: {
+    backgroundColor: 'black',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    marginTop: 15,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  inputContainer: {
+    top:200,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%'
   },
 });
